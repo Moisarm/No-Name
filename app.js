32e05e6df36608bd36ad044d38945ab4f6722a85
Moisarm <v.30247986.mr@gmail.com>
2024-10-25 20:54:55 -0400

    Probando el socket.io

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const express = require("express");
 const app = express();
 
-//const Phaser = require("phaser");
-const path = require("path");
-//const { Server } = require("http");
-
 const serv = require("http").Server(app);
+const io = require("socket.io")(serv, {});
+const path = require("path");
+
+//const Phaser = require("phaser");
+//const { Server } = require("http");
 
 /*---------------Rutas---------------*/
 app.get("/", function (req, res) {
@@ -18,11 +19,22 @@ app.use("/", express.static("./node_modules/phaser/dist"));
 
 console.log("Se inicia el servidor");
 
+/*Puerto que en el cual se ejecutara el servidor*/
 serv.listen(2000);
 
-const io = require("socket.io")(serv, {});
-
+/*Inicio el listening de socket.io */
 io.sockets.on("connection", function (socket) {
   console.log("socket connection");
+
+  /*Sigue siendo listening de lo que envia el cliente, aunque se usa un emit*/
+  socket.on("XD", function (data) {
+    console.log(`Oye ${data.info}`);
+  });
+
+  /*Para emitir mensajes se  utiliza el metodo emit y como en el listening se pasa un nombre de evento, el cual se utiliza para identificar el socket que se quiere notificar*/
+  socket.emit("serverMsg", {
+    msg: "Esto es un mensaje enviado desde el servidor",
+  });
 });
+
 module.exports = app;
